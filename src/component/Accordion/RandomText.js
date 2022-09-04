dbc0283703868b2acdf0261e59378fccd59d29f3
Aatish1111 <aatishkumarmoharana@gmail.com>
2022-09-04 18:20:46 +0530

    Dashboard commit

---
+++ src/component/Accordion/RandomText.js
@@ -0,0 +1,52 @@
+// import { Typography } from "@mui/material";
+// const RandomText = () => {
+//   <Typography variant="p">
+//     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+//     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+//     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+//   </Typography>;
+// };
+
+// export default RandomText;
+
+import React from "react";
+
+const RandomText = () => {
+  return (
+    <div>
+      <ul>
+        <p>
+          1. Participants Must Go through the pdf and resources carefully, which
+          will be provided and should follow them in order to start and comple
+          this Upskilling Program with ease.
+        </p>
+        <p>
+          2. Participants should be careful of the timescale and deadlines of
+          each task. i.e., they should always be prepared with their task before
+          the deadline and submit them in the link provided on the website .
+        </p>
+        <p>
+          3. There is a grading system for the submission of task :- i) If
+          Submitted before the deadline , grading will be done from 100. ii) If
+          Submitted one day after , grading will be done from 70. iii) If
+          Submitted 2 days after , grading will be done from 40. After that ,
+          the link will be closed for that particular task.
+        </p>
+        <p>
+          4. If a participant fails to submit his work on time or his submission
+          gets disqualified by the mentors , then he must focus on following
+          week's tasks . We will provide Certificates and perks after
+          calculating the whole 4 weeks' result. You will be getting enough time
+          to improve, show , try to be consistent throughout the program.
+        </p>
+        <p>
+          5. And the most important thing , participants will get only one
+          chance to submit there task in the link provided on the website. So ,
+          make sure you submit the correct task.
+        </p>
+      </ul>
+    </div>
+  );
+};
+
+export default RandomText;
---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Accordian from '../../component/Accordion/Accordian'
+import Footer from '../../component/Footer/Footer'
+import Navbar from '../../component/Navbar'
+
+const Dashboard = () => {
+  return (
+    <>
+        <Navbar />
+        <Accordian />
+        <Footer />
+    </>
+  )
+}
+
+export default Dashboard

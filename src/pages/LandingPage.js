58aa497c197f6f7442361b9b8ffd1e3db9a8497c
Ashish <ashish2ekka10@gmail.com>
2022-09-05 18:04:48 +0530

    landing page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Slide1 from '../components/Slide1'
+import Slide2 from '../components/Slide2'
+import Slide3 from '../components/Slide3'
+
+function LandingPage() {
+  return (
+    <>
+    {/* <Navbar/> */}
+    <Slide1/>
+    <Slide2/>
+    <Slide3/>
+    </>
+  )
+}
+
+export default LandingPage

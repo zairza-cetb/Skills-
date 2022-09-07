01cf1900d77296f6e30905032ce7f4c5dec705fd
Shuvam Kumar Panda <shuvamkumar2001@gmail.com>
2022-09-08 00:39:04 +0530

    Fix landing page

--- src/components/Slide1.js
+++ src/components/Slide1.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import './Slide1.scss'
+import '../css/Slide1.css'
+import heroImage from "../Assets/images/img1.png"
 
 
 function Slide1() {
   return (
     <div className='slide1-bg'>
       <div className='slide1-content'>
-        <h1>The biggest <span style={{color:'#3370F6'}}>Upskilling</span> program for future geeks.</h1>
-        <p>Get started with some real time projects and get your hands dirty and learn more</p>
-        <div className='slide1-button'>
-          <button className='btn'>Register Now</button>
-          <button className='btn btn2'>Check Domains →</button>
+        <div className='heroContent'>
+          <h1>The biggest <span style={{ color: '#3370F6' }}>Upskilling</span> program for future geeks.</h1>
+          <p>Get started with some real time projects and get your hands dirty and learn more</p>
+          <div className='slide1-button'>
+            <button className='btn'>Register Now</button>
+            <button className='btn btn2'>Check Domains →</button>
+          </div>
         </div>
+        <img src={heroImage} alt="" className='heroImage' />
       </div>
-         
+      <div className='blueContainerTop'></div>
+      <div className='blueContainerBottom'></div>
     </div>
   )
 }
--- src/components/Slide2.js
+++ src/components/Slide2.js
@@ -1,32 +1,32 @@
 import React from 'react'
-import './Slide2.scss'
-import {Slide2data1} from './Slide2data'
+import '../css/Slide2.css'
+import { Slide2data1 } from './Slide2data'
 
 function Slide2() {
   return (
     <div className='wrap'>
-    <div className='content-1'>
-      <h2>What is <span style={{color:'#3370F6'}}>Skills</span><span style={{color:'#EB7F37'}}>++</span> ?</h2>
-      <p>Skills++ is the biggest UPSKILLING program for future geeks and programmers by Zairza. An idea can pop up in the middle of a shower or while going on a walk, or even randomly after hours of thinking. But when it finally comes, the feeling is extraordinary. That feeling can be addictive and Zairza is here to give you that.</p>
-    </div>
-    <div className='content-2'>
-      <h2>Why should you<br/> participate ?</h2>
-      <div className='content-2-items'>
-      {Slide2data1.map((item,index) =>{
-            return(
-                <li key={index} className={item.cName}>
-                  <div className='content-2-wrapper'>
-                    <div className='content-2-icon'><img className='slide-2-icon' src={item.path} alt='404'/></div>
-                    <div>
-                      <div className='content-2-title'>{item.title}</div>
-                      <div className='content-2-detail'style={{color:'#667085'}}>{item.content}</div>
-                    </div>
-                  </div>
-                </li>
-            )         
-        })}
+      <div className='content-1'>
+        <h1>What is <span style={{ color: '#3370F6' }}>Skills</span><span style={{ color: '#EB7F37' }}>++</span>?</h1>
+        <p>Skills++ is the biggest UPSKILLING program for future geeks and programmers by Zairza. An idea can pop up in the middle of a shower or while going on a walk, or even randomly after hours of thinking. But when it finally comes, the feeling is extraordinary. That feeling can be addictive and Zairza is here to give you that.</p>
       </div>
-    </div>
+      <div className='content-2'>
+        <h1>Why should you participate?</h1>
+        <div className='content-2-items'>
+          {Slide2data1.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <div className='content-2-wrapper'>
+                  <div className='content-2-icon'><img className='slide-2-icon' src={item.path} alt='404' /></div>
+                  <div>
+                    <h2 className='content-2-title'>{item.title}</h2>
+                    <p className='content-2-detail' style={{ color: '#667085' }}>{item.content}</p>
+                  </div>
+                </div>
+              </li>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }

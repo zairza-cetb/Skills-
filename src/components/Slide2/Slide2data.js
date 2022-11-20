c8deaa8a97d36a92ba9ab19c0739b05fe1e67df0
unknown <princerupayan1@gmail.com>
2022-11-20 14:01:21 +0530

    Changed slide2data

--- src/components/Slide2/Slide2data.js
+++ src/components/Slide2/Slide2data.js
@@ -5,19 +5,19 @@ import three from '../../Assets/icons/favorite-chart.png'
 export const Slide2data1 = [
     {
         title: 'Certificate',
-        content: 'Get certified from Zairza, which is well recognised by companies',
+        content: 'Get certified from Zairza, and post it in social media to let your recruiter know about your skills.',
         path: one,
         cName: 'item-list'
     },
     {
         title: 'Goodies',
-        content: 'Get certified from Zairza, which is well recognised by companies',
+        content: 'Get amazing goodies from Zairza, and flaunt it in your college',
         path: two,
         cName: 'item-list'
     },
     {
         title: 'Experience',
-        content: 'Get certified from Zairza, which is well recognised by companies',
+        content: 'Get hands-on experience in different fields of technology',
         path: three,
         cName: 'item-list'
     }
